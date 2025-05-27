719017a3b1ac57d63fbba32a5187afce21f43149
ir2-chowdhury <Ifath2.Chowdhury@live.uwe.ac.uk>
2025-05-27 21:13:06 +0100

    Improved output component to format currency values and add error handling for duration input

--- investment-calc-project/src/components/OutputData.jsx
+++ investment-calc-project/src/components/OutputData.jsx
@@ -2,41 +2,43 @@ import React from 'react';
 import { calculateInvestmentResults } from '../util/investments';
 
 const OutputData = ({ inputValue }) => {
-  // Component logic will go here
-  const resultData = calculateInvestmentResults({
+    if (inputValue.duration <= 0) {
+        return <p>Enter a duration greater than 0</p>
+    }
+    // Component logic will go here
+    const resultData = calculateInvestmentResults({
     initialInvestment: +inputValue.initialInvestment,
     annualInvestment: +inputValue.annualInvestment,
     expectedReturn: +inputValue.expectedReturn,
     duration: +inputValue.duration
-  })
+    });
 
-
-  return (
-    <div>
-      <table>
-        <thead>
-            <tr>
-                <th>Year</th>
-                <th>Investment Value</th>
-                <th>Interest (Year)</th>
-                <th>Total Interest</th>
-                <th>Invested Capital</th>
-            </tr>
-        </thead>
-        <tbody>
-            {resultData.map((yearData, index) => (
-                <tr key={index}>
-                <td>{yearData.year}</td>
-                <td>{yearData.investmentValue.toFixed(2)}</td>
-                <td>{yearData.interest.toFixed(2)}</td>
-                <td>{yearData.totalInterest.toFixed(2)}</td>
-                <td>{yearData.investedCapital.toFixed(2)}</td>
-                </tr>
-            ))}
-        </tbody>
-    </table>
-    </div>
-  );
+    return (
+        <div>
+            <table id='result'>
+                <thead id='result-head'>
+                    <tr>
+                    <th>Year</th>
+                    <th>Investment Value</th>
+                    <th>Interest (Year)</th>
+                    <th>Total Interest</th>
+                    <th>Invested Capital</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {resultData.map((yearData, index) => (
+                        <tr key={index}>
+                        <td>{yearData.year}</td>
+                        <td>{yearData.investmentValue.toLocaleString("en-US", {style:"currency", currency:"USD"})}</td>
+                        <td>{yearData.interest.toLocaleString("en-US", {style:"currency", currency:"USD"})}</td>
+                        <td>{yearData.totalInterest.toLocaleString("en-US", {style:"currency", currency:"USD"})}</td>
+                        <td>{yearData.investedCapital.toLocaleString("en-US", {style:"currency", currency:"USD"})}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 };
 
 export default OutputData;
